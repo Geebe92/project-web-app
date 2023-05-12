bdabcd40483939316ec007feab46e4ae855fff54
Geebe92 <b.karnas92@gmail.com>
2023-05-12 11:16:53 +0200

    reszta projektu

--- src/main/java/com/project/webapp/controller/ProjektController.java
+++ src/main/java/com/project/webapp/controller/ProjektController.java
@@ -3,6 +3,7 @@ package com.project.webapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
